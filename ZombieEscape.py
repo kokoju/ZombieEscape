7fc5c3f35b0b4fde801ae56133072b15051b5925
Kokoju <j.bastidas.1@estudiantec.cr>
2025-05-21 21:53:47 -0600

    Inicio visión, arreglar

--- ZombieEscape.py
+++ ZombieEscape.py
@@ -49,7 +49,7 @@ dt = 0  # delta time, utilizado para la física de la tasa de refresco
 
 # Bucle que corre el juego mientras running sea True
 while running:
-    
+    movimiento = False  # Variable para controlar el movimiento del jugador
     # Si el evento detectado es QUIT, se deja de correr el juego
     for evento in event.get():
         if evento.type == QUIT:
@@ -57,6 +57,7 @@ while running:
 
 # CORREGIR EL TEMA DEL % Y LOS INDEX OUT OF RANGE
         elif evento.type == KEYDOWN:
+            movimiento = True  # Se activa el movimiento al presionar una tecla
             # Si se presiona la tecla W, A, S o D, se mueve el jugador en la dirección correspondiente, claramente evitando que se chocque con una pared
             # Gracias a los % len(matriz_prueba) y % len(matriz_prueba[0]), se genera un loop si el jugador intenta salir de la pantalla, como PAC-MAN
             if evento.key == K_w and matriz_prueba[(player_pos[0] - 1) % len(matriz_prueba)][player_pos[1]] != 1:
@@ -75,14 +76,15 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     pantalla.fill("black")
 
-    # Se recorre la matriz por primera vez: esto para poner en 0s en los 4s para actualizar la visión de los enemigos
-    for fila in range(len(matriz_prueba)):
-        for columna in range(len(matriz_prueba[fila])):
-            if matriz_prueba[fila][columna] == 4:
-                draw.rect(pantalla, "yellow", (columna*tamaño_cuadro, fila*tamaño_cuadro, tamaño_cuadro, tamaño_cuadro))
+    if movimiento:
+        # Se recorre la matriz por primera vez: esto para poner en 0s en los 4s para actualizar la visión de los enemigos
+        for fila in range(len(matriz_prueba)):
+            for columna in range(len(matriz_prueba[fila])):
+                if matriz_prueba[fila][columna] == 4:
+                    matriz_prueba[fila][columna] = 0
 
-            # Ahora con todo dibujado, se dibuja el cuadro de la visión del enemigo, hecho como una cruz hasta que choca con un enemigo
-
+    
+    # Ahora con todo dibujado, se dibuja el cuadro de la visión del enemigo, hecho como una cruz hasta que choca con un enemigo
 
     # Con esta construcción, se dibujan los cuadros de la matriz en la pantalla (de arriba para abajo y de izquierda a derecha)
     # DISCLAIMER: El código no funciona correctamente con (fila, columna) en las coords, ya que no se dibujaría bien
@@ -103,8 +105,29 @@ while running:
                 # Anota las coordenadas del jugador
                 player_pos = [fila, columna]
             elif matriz_prueba[fila][columna] == 3:
-                # Dibuja el enemigo
+                # Dibuja al enemigo
                 draw.rect(pantalla, "red", (columna*tamaño_cuadro, fila*tamaño_cuadro, tamaño_cuadro, tamaño_cuadro))
+                A = 1  # Arriba
+                B = 1  # Abajo
+                I = 1  # Izquierda
+                D = 1  # Derecha
+                rango_vision = 3  # Rango de visión del enemigo
+                # Mientras subamos y no haya pared o enemigo, se crea el area de visión
+                while (fila - A) >= 0 and matriz_prueba[fila - A][columna] not in [1, 2, 3] and A <= 3:  
+                    matriz_prueba[fila - A][columna] = 4
+                    A += 1
+                while (fila + B) < len(matriz_prueba) and matriz_prueba[fila + B][columna] not in [1, 2, 3] and B <= 3:
+                    matriz_prueba[fila + B][columna] = 4
+                    B += 1
+                while (columna - I) >= 0 and matriz_prueba[fila][columna - I] not in [1, 2, 3] and I <= 3:
+                    matriz_prueba[fila][columna - I] = 4
+                    I += 1
+                while (columna + D) < len(matriz_prueba[fila]) and matriz_prueba[fila][columna + D] not in [1, 2, 3] and D <= 3:
+                    matriz_prueba[fila][columna + D] = 4
+                    D += 1
+            elif matriz_prueba[fila][columna] == 4:
+                # Dibuja el área de visión del enemigo
+                draw.rect(pantalla, "orange", (columna*tamaño_cuadro, fila*tamaño_cuadro, tamaño_cuadro, tamaño_cuadro))
 
             elif matriz_prueba[fila][columna] == 5:
                 draw.rect(pantalla, "yellow", (columna*tamaño_cuadro, fila*tamaño_cuadro, tamaño_cuadro, tamaño_cuadro))
